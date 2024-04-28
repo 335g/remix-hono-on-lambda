8a5a735fbf3b5ea7fe76cc80ce3ad4d23adcfad1
335g <actionstar619@yahoo.co.jp>
2024-04-28 15:05:02 +0900

    chore: Dockerfile

--- lamdba/my-app/server.ts
+++ lamdba/my-app/server.ts
@@ -1,28 +1,30 @@
-import { Hono } from 'hono';
-import { serveStatic } from '@hono/node-server/serve-static';
-import { remix } from 'remix-hono/handler';
-import { serve } from '@hono/node-server';
+import { Hono } from "hono";
+import { serveStatic } from "@hono/node-server/serve-static";
+import { remix } from "remix-hono/handler";
+import { serve } from "@hono/node-server";
 
 const app = new Hono();
-if (process.env.NODE_ENV !== 'development' || import.meta.env.PROD) {
-  app.use("/assets/*", serveStatic({ root: './build/client' }));
+if (process.env.NODE_ENV !== "development" || import.meta.env.PROD) {
+  app.use("/assets/*", serveStatic({ root: "./build/client" }));
   app.use(async (c, next) => {
-    const serverBuild = await import('./build/server/index.js');
+    const serverBuild = await import("./build/server/index.js");
     return remix({
       build: serverBuild,
-      mode: 'production',
-      getLoadContext(c) {
-        return c.env;
-      }
-    })
+      mode: "production",
+    });
+  });
+
+  serve({
+    fetch: app.fetch,
+    port: 8080,
   });
 } else {
   app.use(async (c, next) => {
-    const build = await import('virtual:remix/server-build');
-    const { createRequestHandler } = await import('@remix-run/node');
-    const handler = createRequestHandler(build, 'development');
+    const build = await import("virtual:remix/server-build");
+    const { createRequestHandler } = await import("@remix-run/node");
+    const handler = createRequestHandler(build, "development");
     return handler(c.req.raw, {});
-  })
+  });
 }
 
 export default app;
