028d6e6b9bfceda7be18b4b8666b9b549e0335c8
335g <actionstar619@yahoo.co.jp>
2024-04-28 14:27:35 +0900

    feat: init shadcn/ui

--- lamdba/my-app/vite.config.ts
+++ lamdba/my-app/vite.config.ts
@@ -1,10 +1,16 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
+import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+	plugins: [remix(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "./app"),
+		},
+	},
 });
